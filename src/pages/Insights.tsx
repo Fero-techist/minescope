9215cd4f5cf9159f450b138f6ab9a13c31e358ea
feranmi <feranmij10@gmail.com>
2026-08-20 09:17:44 +0100

    in

--- src/pages/Insights.tsx
+++ src/pages/Insights.tsx
@@ -2,7 +2,7 @@ import Map from "../assets/map.png";
 
 const Insights = () => {
   return (
-    <div className=" py-10 max-w-7xl mx-auto px-6">
+    <div className=" py-10 max-w-7xl md:h-auto mx-auto px-6">
       <img
         src={Map}
         alt="Map Visualization"
